81f752f50f8ced195acf0f47acf3094f0da83070
vaiserk <ootaviooffreitass@gmail.com>
2025-10-27 17:13:22 -0300

    Commit inicial do projeto Farmácia com Laravel e Tailwind

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Farmácia</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+</head>
+<body>
+    <h1 class="text-3xl font-bold text-blue-600 underline p-10">
+        Olá, Tailwind!
+    </h1>
+
+    <div class="bg-gray-100 p-10 m-10 rounded-lg shadow-lg">
+        <p>Seu sistema de farmácia está funcionando com Laravel e Tailwind!</p>
+    </div>
+</body>
+</html>
